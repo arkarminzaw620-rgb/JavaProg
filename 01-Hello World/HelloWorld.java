5f6758c6d4a3d056154b0b6e0ae62b9a2f1ced85
arkarminzaw620-rgb <arkarminzaw620@gmail.com>
2025-08-21 13:51:24 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -8,5 +8,9 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World");
+        System.out.println("My name is Arar Min Zaw");
+        System.out.println("Course name: Object-orented in programming in Java 1");
+        System.out.println("Course codeL CSC200 A");
+        System.out.println("Lecturer name: Mr Sathish Sarma");
     }
 }
